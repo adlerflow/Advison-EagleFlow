43f4cf93f08fab72ad4fa8c51735d4a68a05c9ea
Constantin Adler <constantin.adler@icloud.com>
2025-05-13 09:14:47 +0200

    Initial Commit

---
+++ EagleFlowTests/EagleFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  EagleFlowTests.swift
+//  EagleFlowTests
+//
+//  Created by adler on 13.05.2025.
+//
+
+import Testing
+
+struct EagleFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
